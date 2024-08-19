dc17d99009f99e310fdeb8ab0bc0704217b0b0b2
SAKTHIVEL <144527896+nameissakthi@users.noreply.github.com>
2024-08-19 21:54:03 +0530

    Update LoginSignup.jsx

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
+  const [loginsign, setLoginsign] = useState("Sign up")
+  const name = useRef()
+
+  function LoginSign(){
+    if(name.current.style.display === 'none'){
+      name.current.style.display = 'block'
+      setLoginsign('Sign up')
+    }else{
+      name.current.style.display = 'none'
+      setLoginsign('Login')
+    }
+    console.log(loginsign)
+  }
+
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{loginsign}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name' />
+          <input type="text" placeholder='Your Name' ref={name} />
           <input type="email" placeholder='Email Address' />
           <input type="password" placeholder='Password' />
         </div>
         <button>Continue</button>
-        <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+        <p className="loginsignup-login">Already have an account? <span><a href="#" onClick={LoginSign}>Login here</a></span></p>
         <div className="loginsignup-agree">
           <input type="checkbox" name='' id='' />
           <p>By continuing, i agree to the terms of use & privacy policy.</p>
